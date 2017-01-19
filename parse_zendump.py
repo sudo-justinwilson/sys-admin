31b0a5f71b73b0648d3a7f020ee8853637fbaa76
sudo-justinwilson <sudo.justin.wilson@gmail.com>
2017-01-19 23:44:45 +1100

    more zenparse

--- parse_zendump.py
+++ parse_zendump.py
@@ -1,6 +1,23 @@
-for item in [x for x in sample.splitlines()[0].split()]:
-    if 'Title' in item:
-            print item
-    if 'ManageIp' in item:
-            print item
 
+# I used a string, but file.readlines() is a list...V
+zendump = open('zendump.csv').readlines()
+
+ip_list = []
+devices = []
+
+for item in [element for element in [line.split(',') for line in sample.splitlines()] if len(element) > 1]:
+	ip_list +=      [field.split('=')[1].replace("'", "") for field in item if 'ManageIp' in field]
+	devices +=      [field.split('=')[1].replace("'", "").rstrip('.barangaroo.com.au') for field in item if 'setTitle' in field]
+
+device = dict(zip(devices, ip_list))
+
+-----
+
+
+
+
+for item in [element for element in [line.split(',') for line in open('zendump.csv').readlines().splitlines()] if len(element) > 1]:
+	ip_list +=      [field.split('=')[1].replace("'", "") for field in item if 'ManageIp' in field]
+	devices +=      [field.split('=')[1].replace("'", "").rstrip('.barangaroo.com.au') for field in item if 'setTitle' in field]
+
+device = dict(zip(devices, ip_list))
