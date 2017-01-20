4c9d4e971a430e2bbad106283123aa527fa58188
sudo-justinwilson <sudo.justin.wilson@gmail.com>
2017-01-20 16:19:49 +1100

    create parse zendump function

--- parse_zendump.py
+++ parse_zendump.py
@@ -1,23 +1,23 @@
+# this is to parse a zenoss export csv file:
+def parse_zendump(file_in, file_out):
+    """
+    This takes a zendump from a Zenoss instance, and parses it for device names, and IP addresses.
+    """
+    # create empty lists to store the values in:
+    ip_list = []
+    devices = []
+    
+    
+    # the data is quite nested, and I am only interested in the device name and IP address:
+    for item in [element for element in [line.split(',') for line in open(zen_file).readlines()] if len(element) > 1]: 
+        ip_list +=      [field.split('=')[1].replace("'", "") for field in item if 'ManageIp' in field]
+        devices +=      [field.split('=')[1].replace("'", "").rstrip('.barangaroo.com.au') for field in item if 'setTitle' in field]
+    
+    # create a dict out of the device names and IP addresses:
+    devices_dict = dict(zip(tmp_devices, ip_tmp)) 
+    
+    # write the dict to file_out:
+    with open(file_out, 'w') as output:
+        for k, v in devices_list.viewitems():
+            output.write(k + " : " + v + "\n")
 
-# I used a string, but file.readlines() is a list...V
-zendump = open('zendump.csv').readlines()
-
-ip_list = []
-devices = []
-
-for item in [element for element in [line.split(',') for line in sample.splitlines()] if len(element) > 1]:
-	ip_list +=      [field.split('=')[1].replace("'", "") for field in item if 'ManageIp' in field]
-	devices +=      [field.split('=')[1].replace("'", "").rstrip('.barangaroo.com.au') for field in item if 'setTitle' in field]
-
-device = dict(zip(devices, ip_list))
-
------
-
-
-
-
-for item in [element for element in [line.split(',') for line in open('zendump.csv').readlines().splitlines()] if len(element) > 1]:
-	ip_list +=      [field.split('=')[1].replace("'", "") for field in item if 'ManageIp' in field]
-	devices +=      [field.split('=')[1].replace("'", "").rstrip('.barangaroo.com.au') for field in item if 'setTitle' in field]
-
-device = dict(zip(devices, ip_list))
