629b2a4ad6fddf91676e26a4770b9c0edcb42184
sudo-justinwilson <sudo.justin.wilson@gmail.com>
2017-08-25 13:52:55 +1000

    Remove comments, etc from zfs-backups.sh

--- bash/zfs-backups.sh
+++ bash/zfs-backups.sh
@@ -12,12 +12,4 @@ if [[ -z $1 ]]; then
 else
     export TARGET=$1;
     for DIR in `zfs list | awk '{ print $5 }' | grep -v none | grep -v MOUNTPOINT`; do echo "tar cvfz /${TARGET}/$(echo $DIR | replace '/' '-').tgz $DIR"; done;
-    #for DIR in `zfs list | awk '{ print $5 }' | grep -v none | grep -v MOUNTPOINT`; do tar cvfz /zfs-backups/$(echo $DIR | replace '/' '-').tgz $DIR; done;
-    #echo $DIR;
 fi
-
-#    for DIR in `zfs list | awk '{ print $5 }' | grep -v none | grep -v MOUNTPOINT`; do tar cvfz /zfs-backups/$(echo $DIR | replace '/' '-').tgz $DIR; done
-#     for DIR in `zfs list | awk '{ print $5 }' | grep -v none | grep -v MOUNTPOINT`; do tar cvfz /zfs-backups/$(echo $DIR | replace '/' '-').tgz $DIR; done
-#fi
-#
-# for DIR in `zfs list | awk '{ print $5 }' | grep -v none | grep -v MOUNTPOINT`; do tar cvfz /zfs-backups/$(echo $DIR | replace '/' '-').tgz $DIR; done
